9b1f4834bf3ddf46e56e113ec66d4521e1b0305c
Miwa M <mwmlln@gmail.com>
2023-06-23 16:28:34 +0100

    initial commit with Ionic skelton project

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyNews',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
